fc55ac801904d9200509c444828f1aceba85f417
Daniel <4458323+dherskowitz@users.noreply.github.com>
2020-03-12 16:19:01 +0200

    add pwa plugin

--- app/gridsome.config.js
+++ app/gridsome.config.js
@@ -6,5 +6,23 @@
 
 module.exports = {
   siteName: 'What Time is Shabbos?',
-  plugins: []
+  plugins: [{
+    use: 'gridsome-plugin-pwa',
+    options: {
+      title: 'What Time Is Shabbos',
+      startUrl: '/',
+      display: 'standalone',
+      statusBarStyle: 'default',
+      manifestPath: 'manifest.json',
+      disableServiceWorker: true,
+      serviceWorkerPath: 'service-worker.js',
+      cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+      shortName: 'Shabbos Time?',
+      themeColor: '#666600',
+      backgroundColor: '#ffffff',
+      icon: 'src/favicon.png', // must be provided like 'src/favicon.png'
+      msTileImage: '',
+      msTileColor: '#666600'
+    }
+  }]
 }
